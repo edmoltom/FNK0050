5790502d76e1d115e8638272cfc55317feb0faa3
Eduardo Molina Tomás <eduardomolinatomas@gmail.com>
2025-07-30 16:10:36 +0200

    Added hello world test code

---
+++ app/hello_world.py
@@ -0,0 +1,12 @@
+from Action import Action
+
+def main():
+    print("Hello!! (●'◡'●)")
+
+    action = Action()
+
+    action.hello()
+
+# Opcional, para ejecutar también directamente
+if __name__ == "__main__":
+    main()
