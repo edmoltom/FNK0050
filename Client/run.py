3357069b81d4a715b663e7e7002880bc1c8e1168
Eduardo Molina Tomás <eduardomolinatomas@gmail.com>
2025-08-05 11:04:58 +0200

    Added script to launch different codes from client side

---
+++ Client/run.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+project_root = os.path.dirname(os.path.abspath(__file__))
+
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+for folder in ['network', 'test_codes']:
+    folder_path = os.path.join(project_root, folder)
+    if folder_path not in sys.path:
+        sys.path.insert(0, folder_path)
+  
+from test_ws_client import main
+
+if __name__ == '__main__':
+    main()
