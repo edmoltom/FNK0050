9d5c1fb4dff57de12a27fdc2b7af10fdf5694aac
Eduardo Molina Tomás <eduardomolinatomas@gmail.com>
2025-08-05 17:26:21 +0200

    Added GUI test implemented with PyQT

--- Client/run.py
+++ Client/run.py
@@ -11,7 +11,8 @@ for folder in ['network', 'test_codes']:
     if folder_path not in sys.path:
         sys.path.insert(0, folder_path)
   
-from test_ws_client import main
+#from test_ws_client import main
+from hello_world_gui import main
 
 if __name__ == '__main__':
     main()
