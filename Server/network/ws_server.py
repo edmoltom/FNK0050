b7de8b5a76606ae2e89ddafd2083b427b70fba2d
Eduardo Molina Tomás <eduardomolinatomas@gmail.com>
2025-08-07 10:57:08 +0200

    Added ping response

--- Server/network/ws_server.py
+++ Server/network/ws_server.py
@@ -1,15 +1,22 @@
-
 import asyncio
 import websockets
+import socket
 import json
-import base64
-from io import BytesIO
-from PIL import Image
 
 from core.Camera import Camera
 
 camera = Camera()
 camera.start_periodic_capture(interval=1.0)
+
+def get_local_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+        s.close()
+        return ip
+    except:
+        return "127.0.0.1"
 
 async def handler(websocket):
 
@@ -19,12 +26,11 @@ async def handler(websocket):
         try:
             data = json.loads(message)
             cmd = data.get("cmd")
-            if cmd == "capture":
-                img_str = camera.get_last_processed_encoded()
+            if cmd == "ping":
                 response = {
                     "status": "ok",
-                    "type": "image",
-                    "data": img_str
+                    "type": "text",
+                    "data": "pong"
                 }
             elif cmd == "set_mode":
                 mode = data.get("mode")
@@ -50,7 +56,7 @@ async def handler(websocket):
 
 async def start_ws_server_async():
 
-    print("WebSocket listening in ws://0.0.0.0:8765 ...")
+    print(f"WebSocket listening in ws://{get_local_ip()}:8765 ...")
     async with websockets.serve(handler, "0.0.0.0", 8765):
         try:
             await asyncio.Future()  # keep server running
