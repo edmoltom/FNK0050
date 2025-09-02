18c0d5a247c76cce1a98e7c938e2f2420f77d828
Eduardo Molina Tomás <eduardomolinatomas@gmail.com>
2025-09-02 09:13:52 +0200

    Refactor server package setup

--- Server/run.py
+++ Server/run.py
@@ -1,17 +1,4 @@
-import os
-import sys
-
-project_root = os.path.dirname(os.path.abspath(__file__))
-
-if project_root not in sys.path:
-    sys.path.insert(0, project_root)
-
-for folder in ["core", "lib", "test_codes", "network"]:
-    folder_path = os.path.join(project_root, folder)
-    if folder_path not in sys.path:
-        sys.path.insert(0, folder_path)
-
-from app.application import Application
+from Server.app.application import Application
 
 
 def main() -> None:
